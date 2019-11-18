e80838ca3dbed56b052e03933f6cd15fc6b078f4
maciewheeler <53661238+maciewheeler@users.noreply.github.com>
2019-11-18 11:40:37 -0500

    Update Delta.java

--- src/Delta.java
+++ src/Delta.java
@@ -1,10 +1,12 @@
 /**
- *
+ * A class for the Delta airline.
  *
  * @author Macie Wheeler and Keya Mahtani
  * @version November 18, 2019
  */
+
 public class Delta implements Airline {
+
     public String getName() {
         return "Delta";
     }
