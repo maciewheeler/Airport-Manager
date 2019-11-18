8fc2ef82f7548521539718f966d1485d8fd3bc35
kmahtani <54550690+kmahtani@users.noreply.github.com>
2019-11-18 11:53:30 -0500

    Update Alaska.java

--- src/Alaska.java
+++ src/Alaska.java
@@ -4,11 +4,18 @@
  * @author Macie Wheeler and Keya Mahtani
  * @version November 18, 2019
  */
-
 public class Alaska implements Airline {
+    int passengerCounter = 0;
+    boolean full = false; //true when flight is full
 
+    public void statusChanger() {
+        if (passengerCounter >= passengerMax) {
+            full = true;
+        }
+    }
     @Override
     public String getName() {
         return "Alaska";
     }
+
 }
