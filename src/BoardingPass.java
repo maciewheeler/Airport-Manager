99b8cbccd91a177f3ddfb3e5cfe80b358f34cf40
maciewheeler <53661238+maciewheeler@users.noreply.github.com>
2019-11-18 11:35:15 -0500

    Update BoardingPass.java

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -1,9 +1,27 @@
 /**
- *
+ * A class that creates a boarding pass for each customer.
  *
  * @author Macie Wheeler and Keya Mahtani
  * @version November 18, 2019
  */
 
-public class BoardingPass {
+public class BoardingPass extends Passenger implements Airline {
+
+    public BoardingPass(String firstName, String lastName, int age) {
+        super(firstName, lastName, age);
+    }
+
+    public String writeBoardingPass() {
+        String boardingPass = "----------------------------------------" + "\n" +
+                "BOARDING PASS FOR FLIGHT 18000 WITH " + "\n" +
+                "PASSENGER FIRST NAME: " + super.getFirstName() + "\n" +
+                "PASSENGER LAST NAME: " + super.getLastName() + "\n" +
+                "PASSENGER AGE: " + super.getAge() + "\n" +
+                "You can now begin boarding at gate " + "\n" + 
+                "----------------------------------------";
+        return boardingPass;
+    }
 }
+
+//possibly have this extend passenger and gate????
+//and have implement airline???
