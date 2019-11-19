9d5573b8b7aebe65b7b7ee146edc1461f8a33ffa
maciewheeler <53661238+maciewheeler@users.noreply.github.com>
2019-11-19 18:33:58 -0500

    Update BoardingPass.java

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -1,10 +1,3 @@
-/**
- * A class that creates a boarding pass for each customer.
- *
- * @author Macie Wheeler and Keya Mahtani
- * @version November 18, 2019
- */
-
 import java.io.Serializable;
 
 /**
@@ -16,36 +9,36 @@ import java.io.Serializable;
 
 public class BoardingPass implements Serializable {
 
-    Passenger passenger;
-    Airline airline;
-    Gate gate;
-    String gateToString;
-    String airlineName;
+    private Passenger passenger;
+    private Airline airline;
+    private Gate gate;
+    private String gateToString;
+    private String airlineName;
+    
     public BoardingPass(Passenger p, Airline a) {
         this.passenger = p;
         this.airline = a;
+        
         if (airline instanceof Delta) {
             this.gate = Delta.getDeltaGate();
             this.gateToString = Delta.getDeltaGateString();
             this.airlineName = Delta.getNameOfAirline();
-        }
-        if (airline instanceof Southwest) {
+        } else if (airline instanceof Southwest) {
             this.gate = Southwest.getSouthwestGate();
             this.gateToString = Southwest.getSouthwestGateToString();
             this.airlineName = Southwest.getNameOfAirline();
-        }
-        if (airline instanceof Alaska) {
+        } else if (airline instanceof Alaska) {
             this.gate = Alaska.getAlaskaGate();
             this.gateToString = Alaska.getAlaskaGateToString();
             this.airlineName = Alaska.getNameOfAirline();
         }
-        
+
     }
-    
-    
+
+
     public String writeBoardingPass() {
         String boardingPass = "----------------------------------------" + "\n" +
-                "BOARDING PASS FOR FLIGHT 18000 WITH " + airlineName + "\n" + 
+                "BOARDING PASS FOR FLIGHT 18000 WITH " + airlineName + "\n" +
                 "PASSENGER FIRST NAME: " + passenger.getFirstName() + "\n" +
                 "PASSENGER LAST NAME: " + passenger.getLastName() + "\n" +
                 "PASSENGER AGE: " + passenger.getAge() + "\n" +
