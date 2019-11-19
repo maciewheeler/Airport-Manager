e554262da8f87450b17e0c9620ecab722c208103
kmahtani <54550690+kmahtani@users.noreply.github.com>
2019-11-18 23:06:44 -0500

    Update Delta.java

--- src/Delta.java
+++ src/Delta.java
@@ -1,21 +1,29 @@
+  
 /**
  * A class for the Delta airline.
  *
  * @author Macie Wheeler and Keya Mahtani
  * @version November 18, 2019
  */
-
 public class Delta implements Airline {
-    int passengerCounter = 0;
-    boolean full = false; //true when flight is full
+   private boolean full = false; //true when flight is full
+    private Gate gate;
+    private int passengerCounter = passengers.size();
     final int flightNumber = 18000;
-
     public void statusChanger() {
         if (passengerCounter >= passengerMax) {
             full = true;
         }
     }
-
+    public int getPassengerCounter() {
+        return passengerCounter;
+    }
+    public void setGate() {
+        if (passengerCounter > 0) {
+            gate = new Gate (this.gate.gateNumber, this.gate.terminal);
+        }
+    }
+    @Override
     public String getNameOfAirline() {
         return "Delta";
     }
