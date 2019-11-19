e6f008bfd1669c0e0be1fbc6fb289c16a033109c
maciewheeler <53661238+maciewheeler@users.noreply.github.com>
2019-11-18 19:18:44 -0500

    Update Delta.java

--- src/Delta.java
+++ src/Delta.java
@@ -8,14 +8,19 @@
 public class Delta implements Airline {
     int passengerCounter = 0;
     boolean full = false; //true when flight is full
+    final int flightNumber = 18000;
 
     public void statusChanger() {
         if (passengerCounter >= passengerMax) {
             full = true;
         }
     }
-    
-    public String getName() {
+
+    public String getNameOfAirline() {
         return "Delta";
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
     }
 }
