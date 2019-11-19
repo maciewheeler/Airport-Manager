c34cee7a7b9aa84b2a1a8fdfd0f362ce09347458
kmahtani <54550690+kmahtani@users.noreply.github.com>
2019-11-19 13:32:52 -0500

    Update BoardingPass.java

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -5,16 +5,29 @@
  * @version November 18, 2019
  */
 
-public class BoardingPass extends Passenger implements Airline {
+import java.io.Serializable;
 
+/**
+ * A class that creates a boarding pass for each customer.
+ *
+ * @author Macie Wheeler and Keya Mahtani
+ * @version November 18, 2019
+ */
+
+public class BoardingPass implements Serializable {
+
+    Passenger passenger;
+    Airline airline;
+    Gate gate;
     private String airlineName;
     private final int flightNumber = 18000;
 
-    public BoardingPass(String firstName, String lastName, int age) {
-        super(firstName, lastName, age);
-        this.airlineName = getNameOfAirline();
-    }
 
+    public BoardingPass(Passenger p, Airline a, Gate g) {
+        this.passenger = p;
+        this.airline = a;
+        this.gate = g;
+    }
     public String getNameOfAirline() {
         return airlineName;
     }
@@ -22,14 +35,19 @@ public class BoardingPass extends Passenger implements Airline {
     public int getFlightNumber() {
         return flightNumber;
     }
+    public void addPassenger(Passenger p) {
+        this.airline.passengers.add(p);
+    }
+
+
 
     public String writeBoardingPass() {
         String boardingPass = "----------------------------------------" + "\n" +
-                "BOARDING PASS FOR FLIGHT " + getFlightNumber() + " WITH " + getNameOfAirline() + "\n" +
-                "PASSENGER FIRST NAME: " + super.getFirstName() + "\n" +
-                "PASSENGER LAST NAME: " + super.getLastName() + "\n" +
-                "PASSENGER AGE: " + super.getAge() + "\n" +
-                "You can now begin boarding at gate " + "\n" +
+                "BOARDING PASS FOR FLIGHT 18000 WITH " + "\n" +
+                "PASSENGER FIRST NAME: " + passenger.getFirstName() + "\n" +
+                "PASSENGER LAST NAME: " + passenger.getLastName() + "\n" +
+                "PASSENGER AGE: " + passenger.getAge() + "\n" +
+                "You can now begin boarding at gate " + gate.getGate() + "\n" +
                 "----------------------------------------";
         return boardingPass;
     }
