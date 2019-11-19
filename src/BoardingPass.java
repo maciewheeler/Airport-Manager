7c0d1bb2e67a44a1742e0e17aa034ddd314eb61d
maciewheeler <53661238+maciewheeler@users.noreply.github.com>
2019-11-18 19:18:12 -0500

    Update BoardingPass.java

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -8,19 +8,24 @@
 public class BoardingPass extends Passenger implements Airline {
 
     private String airlineName;
-    
+    private final int flightNumber = 18000;
+
     public BoardingPass(String firstName, String lastName, int age) {
         super(firstName, lastName, age);
-        this.airlineName = getName();
+        this.airlineName = getNameOfAirline();
     }
 
-    public String getName() {
+    public String getNameOfAirline() {
         return airlineName;
     }
-    
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
     public String writeBoardingPass() {
         String boardingPass = "----------------------------------------" + "\n" +
-                "BOARDING PASS FOR FLIGHT 18000 WITH " + getName() + "\n" +
+                "BOARDING PASS FOR FLIGHT " + getFlightNumber() + " WITH " + getNameOfAirline() + "\n" +
                 "PASSENGER FIRST NAME: " + super.getFirstName() + "\n" +
                 "PASSENGER LAST NAME: " + super.getLastName() + "\n" +
                 "PASSENGER AGE: " + super.getAge() + "\n" +
