cee28e32a38ae3b0e127492438cd24b33ff62426
kmahtani <54550690+kmahtani@users.noreply.github.com>
2019-11-22 15:11:00 -0500

    Update Delta.java

--- src/Delta.java
+++ src/Delta.java
@@ -43,16 +43,15 @@ public class Delta implements Airline {
     public static String getDeltaGateString() {
         return deltaGate.getGate();
     }
-
     public static Gate getDeltaGate() {
         return deltaGate;
     }
 
     public static String getAirlineMessage() {
-        return "Delta Airlines is proud to be one of the five premier Airlines at Purdue University. " + "\n" +
+        return "<html><center>Delta Airlines is proud to be one of the five premier Airlines at Purdue University. " + "\n" +
                 "We offer extremely exceptional services, with free limited WiFi for all customers." + "\n" +
                 "Passengers who use T-Mobile as a cell phone carrier get additional benefits." + "\n" +
                 "We are also happy to offer power outlets in each seat for passenger use." + "\n" +
-                "We hope you choose to fly Delta as your next Airline.";
+                "We hope you choose to fly Delta as your next Airline.</center></html>";
     }
 }
