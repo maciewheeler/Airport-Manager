71ed9f772f98dc52aa240212f068f29075d858cc
kmahtani <54550690+kmahtani@users.noreply.github.com>
2019-11-22 15:10:05 -0500

    Update Alaska.java

--- src/Alaska.java
+++ src/Alaska.java
@@ -43,18 +43,17 @@ public class Alaska implements Airline {
     public static String getAlaskaGateToString() {
         return alaskaGate.getGate();
     }
-
     public static Gate getAlaskaGate() {
         return alaskaGate;
     }
 
     public static String getAirlineMessage() {
-        return "Alaska Airlines is proud to serve the strong and knowledgeable Boilermakers from Purdue" +
+        return "<html><center>Alaska Airlines is proud to serve the strong and knowledgeable Boilermakers from Purdue" +
                 " University." + "\n" +
                 "We primarily westward, and often have stops in Alaska and California." + "\n" +
                 "We have first class amenities, even in coach class." + "\n" +
                 "We provide fun snacks such as pretzels and goldfish." + "\n" +
                 "We also have comfortable seats and free WiFi." + "\n" +
-                "We hope you choose Alaska Airlines for your next itinerary.";
+                "We hope you choose Alaska Airlines for your next itinerary.</center></html>";
     }
 }
