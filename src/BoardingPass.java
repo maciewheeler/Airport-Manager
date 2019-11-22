dd83643a64c1328a096a63ffeb7a005679236dda
kmahtani <54550690+kmahtani@users.noreply.github.com>
2019-11-22 15:33:16 -0500

    Update BoardingPass.java

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -1,3 +1,10 @@
+/**
+ * A class that creates a boarding pass for each customer.
+ *
+ * @author Macie Wheeler and Keya Mahtani
+ * @version November 18, 2019
+ */
+
 import java.io.Serializable;
 
 /**
@@ -10,24 +17,24 @@ import java.io.Serializable;
 public class BoardingPass implements Serializable {
 
     private Passenger passenger;
-    private String airline;
+    private String  airline;
     private Gate gate;
     private String gateToString;
     private String airlineName;
-
-    public BoardingPass(Passenger p, String a) {
+    public BoardingPass(Passenger p, String  a) {
         this.passenger = p;
         this.airline = a;
-
         if (airline.equals("Delta")) {
             this.gate = Delta.getDeltaGate();
             this.gateToString = Delta.getDeltaGateString();
             this.airlineName = Delta.getNameOfAirline();
-        } else if (airline.equals("Southwest")) {
+        }
+        if (airline.equals("Southwest")) {
             this.gate = Southwest.getSouthwestGate();
             this.gateToString = Southwest.getSouthwestGateToString();
             this.airlineName = Southwest.getNameOfAirline();
-        } else if (airline.equals("Alaska")) {
+        }
+        if (airline.equals("Alaska")) {
             this.gate = Alaska.getAlaskaGate();
             this.gateToString = Alaska.getAlaskaGateToString();
             this.airlineName = Alaska.getNameOfAirline();
@@ -37,13 +44,13 @@ public class BoardingPass implements Serializable {
 
 
     public String writeBoardingPass() {
-        String boardingPass = "----------------------------------------" + "\n" +
-                "BOARDING PASS FOR FLIGHT 18000 WITH " + airlineName + "\n" +
-                "PASSENGER FIRST NAME: " + passenger.getFirstName() + "\n" +
-                "PASSENGER LAST NAME: " + passenger.getLastName() + "\n" +
-                "PASSENGER AGE: " + passenger.getAge() + "\n" +
-                "You can now begin boarding at gate " + gateToString + "\n" +
-                "----------------------------------------";
+        String boardingPass = "<html><center>------------------------------------------" + "<br>" +
+                "BOARDING PASS FOR FLIGHT 18000 WITH " + airlineName + "<br>" +
+                "PASSENGER FIRST NAME: " + passenger.getFirstName() + "<br>" +
+                "PASSENGER LAST NAME: " + passenger.getLastName() + "<br>" +
+                "PASSENGER AGE: " + passenger.getAge() + "<br>" +
+                "You can now begin boarding at gate " + gateToString + "<br>" +
+                "----------------------------------------</center></html>";
         return boardingPass;
     }
 }
