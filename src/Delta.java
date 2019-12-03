d548fcb9bd9fbfa318bd838f97181a09b8c87e32
maciewheeler <53661238+maciewheeler@users.noreply.github.com>
2019-12-02 22:44:21 -0500

    Update Delta.java

--- src/Delta.java
+++ src/Delta.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Delta implements Airline {
 
     private static ArrayList<Passenger> deltaPassengers = new ArrayList<>();
-    private final static Gate deltaGate = new Gate();
+    private final static Gate DELTA_GATE = new Gate();
     private static String nameOfAirline = "Delta Airlines";
 
     public static void addPassenger(Passenger passenger) {
@@ -25,15 +25,15 @@ public class Delta implements Airline {
     }
 
     public int getMaxPassenger() {
-        return passengerMax;
+        return PASSENGER_MAX;
     }
 
     public int getFlightNumber() {
-        return flightNumber;
+        return FLIGHT_NUMBER;
     }
 
     public int getAvailableSeats() {
-        return passengerMax - deltaPassengers.size();
+        return PASSENGER_MAX - deltaPassengers.size();
     }
 
     public static String getNameOfAirline() {
@@ -41,16 +41,16 @@ public class Delta implements Airline {
     }
 
     public final static String getDeltaGateString() {
-        return deltaGate.getGate();
+        return DELTA_GATE.getGate();
     }
     public final static Gate getDeltaGate() {
-        return deltaGate;
+        return DELTA_GATE;
     }
 
     public static String getAirlineMessage() {
-        return "<html><center>Delta Airlines is proud to be one of the five premier Airlines at Purdue University. " + "<br>" +
-                "We offer extremely exceptional services, with free limited WiFi for all customers." + "<br>" +
-                "Passengers who use T-Mobile as a cell phone carrier get additional benefits." + "<br>" +
+        return "<html><center>Delta Airlines is proud to be one of the five premier Airlines at Purdue University. "
+                + "<br>" + "We offer extremely exceptional services, with free limited WiFi for all customers." +
+                "<br>" + "Passengers who use T-Mobile as a cell phone carrier get additional benefits." + "<br>" +
                 "We are also happy to offer power outlets in each seat for passenger use." + "<br>" +
                 "We hope you choose to fly Delta as your next Airline.</center></html>";
     }
